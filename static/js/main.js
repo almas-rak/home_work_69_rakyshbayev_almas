0c79ef5256bc7fa512c2daa0f0f931c68919468f
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-04-14 01:13:25 +0600

    Задание 2

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,60 @@
-const url = 'http://127.0.0.1:8000'
+const url = 'http://localhost:8000'
+let addBtn = $('#add');
+let subtractBtn = $('#subtract');
+let multiplyBtn = $('#multiply');
+let divideBtn = $('#divide')
+let inputA = $('#input_A')[0]
+let inputB = $('#input_B')[0]
+let result = $('#result')
+
+
+function add(a, b, oper) {
+    let token = $("[name=csrfmiddlewaretoken]").val();
+    let urlRequest = url + '/add/'
+    switch (oper) {
+        case 'add':
+            break
+        case 'subtract':
+            urlRequest = url + '/subtract/';
+            break
+        case 'multiply':
+            urlRequest = url + '/multiply/';
+            break
+        case 'divide':
+            urlRequest = url + '/divide/';
+            break
+    }
+    $.ajax({
+        url: urlRequest,
+        type: 'POST',
+        data: JSON.stringify({
+            "A": a,
+            "B": b
+        }),
+        contentType: "application/json",
+        dataType: 'json',
+        beforeSend: function (xhr, settings) {
+            xhr.setRequestHeader("X-CSRFToken", token);
+        },
+        success: function (data) {
+            result.text(`Результат: ${data.answer}`)
+        },
+        error: function (xhr, status, error) {
+            result.text(`Результат: ${xhr.responseJSON.error}`);
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    })
+
+}
+let test = function (event) {
+    event.preventDefault();
+    let oper = event.currentTarget.id;
+    add(inputA.value, inputB.value, oper)
+}
+
+addBtn.on('click', test);
+subtractBtn.on('click', test);
+multiplyBtn.on('click', test);
+divideBtn.on('click', test);
